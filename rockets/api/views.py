d7ae9c55ed5197044a420654c2019ade2bd05fe4
Karol Chmiel <karolch@macbook.pro>
2021-08-26 16:34:54 +0200

    Initial commit

---
+++ rockets/api/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+
+
+class CoresViewSet(viewsets.ViewSet):
+
+    def retrieve(self, request, pk=None):
+        # validate request params
+        # fetch most used cores
+        # return data
+        pass
---
+++ spacex_api_service/service.py
@@ -0,0 +1,18 @@
+import json
+import urllib
+
+
+def fetch_cores_data(data):
+    # create query using data
+    # create request
+    # parse response
+    # return result
+    pass
+
+
+def get_cores_data(data):
+    cores = fetch_cores_data(data)  # fetch data from external api
+    # calculate overall payload mass for each core
+    # sort cores by reused times
+    # return list
+    pass
